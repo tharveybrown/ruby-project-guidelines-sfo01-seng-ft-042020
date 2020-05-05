bf94aa1e4cc8bb2c1dde0619b6b7ac2a9d12f59b
tharveybrown <tharveybrown@gmail.com>
2020-05-05 16:00:20 -0700

    remove beer functions from user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,6 @@ class User < ActiveRecord::Base
   has_many :beers, through: :reviews
   
 
-  #create methods to be able to sort by category(name, abv, rating)
-  def sort_beers_by_category(category)
-    #list all beers by category, give the user the option for ASC or DESC
-  end
-
-  #create methods to be able to search by(name, abv)
-  def search_beer_by_name(beer_name)
-    #once user types in beer name, returns data(name, desc, abv)
-    #set it so that if a user doesn't input full name of beer, would return list of all the beers that include whatever user
-    #had input
-    #if no matches, return "No matching results"
-  end
-
-  
-
 
   def review_beer
     beer = self.select_beer
@@ -53,13 +38,11 @@ class User < ActiveRecord::Base
     description = gets.chomp
     puts "How would you rate your drink? Enter anything between 1-10"
     rating = gets.chomp.to_f
-    # binding.pry
     Review.update(review.id, description: description, rating: rating, beer_id: beer.id)
   end
 
   def find_beer_to_review
     find_new_beer = PROMPT.yes?("This beer doesn't exist! Would you like to view some random beer names?")
-    # binding.pry
     if !find_new_beer
       self.review_beer
     else
