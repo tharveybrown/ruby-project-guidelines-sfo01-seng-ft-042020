9309e76dfa0bb89c61e10496b7dd2256b4f7eaab
tharveybrown <tharveybrown@gmail.com>
2020-05-05 10:15:39 -0700

    cleanup

--- app/adapter/punk_beer.rb
+++ app/adapter/punk_beer.rb
@@ -8,29 +8,16 @@ class PunkBeer
     response = RestClient.get(url)
     json_response = JSON.parse(response)
     
-    # response_body = response.body
     
     beers = json_response.map do |beer|
       { name: beer["name"], 
         description: beer["description"],
         abv: beer["abv"]
         # food_pairing: beer["food_pairing"]
-
       }
     end
     
-    # books = json_response["items"].map do |item| 
-    #   {description: item["volumeInfo"]["description"],
-    #   title: item["volumeInfo"]["title"]}
-    # end
-    # binding.pry
   end
   
 end
 
-PunkBeer.get_data
-
-# name
-# description
-# abv
-#food_pairing
