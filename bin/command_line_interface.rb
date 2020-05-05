57e8935e4f4f241329210f463febd349eb3ade94
Seanbon0611 <seand0611@gmail.com>
2020-05-05 11:34:36 -0700

    added feature to check if user exists, if not prompt to create user

--- bin/command_line_interface.rb
+++ bin/command_line_interface.rb
@@ -1,17 +1,30 @@
 ### these methods should be within a class.. i.e. Welcome class
+require_relative  '../app/models/user'
+class Welcome 
 
-def welcome
-  # puts out a welcome message here!
-  puts "welcome"
-  puts "enter your email"
-  email = gets.chomp 
-  user_email_exists?(email)
+  def get_email
+    # puts out a welcome message here!
+    puts "welcome"
+    puts "enter your email"
+    email = gets.chomp 
+  end
+  
+  def find_or_create_user(user_email)
+    user = User.find_by(email: user_email)
+    if !user 
+      sign_up(user_email)
+    end
+
+    # Person.find_by(name: 'Spartacus', rating: 4)
+  end
+
+  def sign_up(user_email)
+    puts "Cheers! Whats your name?"
+    name = gets.chomp
+    User.create({ name: name, email: user_email})
+  end
 end
 
-def user_email_exists?(user_email)
-  user = User.find_by(email: user_email)
-  if user
-  # Person.find_by(name: 'Spartacus', rating: 4)
-end
+
 
 # welcome
--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 
 require_relative '../config/environment.rb'
+require_relative 'command_line_interface.rb'
 
 puts "HELLO WORLD"
-welcome
+welcome = Welcome.new
+email = welcome.get_email
+user = welcome.find_or_create_user(email)
 
+binding.pry
 ## TODO
 # welcome = Welcome.new
 # welcome.get_email
