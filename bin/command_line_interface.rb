7b391088c8e5da43628642f06600550de33a11b3
Seanbon0611 <seand0611@gmail.com>
2020-05-05 17:13:44 -0700

    added search_by_name/abv method, updated my_revews to return values of review obj, created user_reveiws which returns values of reviews by beer

--- bin/command_line_interface.rb
+++ bin/command_line_interface.rb
@@ -45,12 +45,25 @@ end
 
 class Menu
   
+  def self.main_menu(user)
+    choices = {'write a review' => 1, 'find a beer to drink' => 2, 'view_my_reviews' => 3, 'exit' => 4}
+
+
+  end
   def choose_what_to_do
     prompt = TTY::Prompt.new
-    
     choices = {'write a review' => 1, 'find a beer to drink' => 2, 'exit' => 3}
-    
-    prompt.select("Choose your destiny?", choices)
+    selection = PROMPT.select("Choose your destiny?", choices)
+    case selection
+    when 1
+      user.review_beer
+    when 2
+      user.find_beer
+    when 3
+      # todo: add my reviews
+      user.my_reviews
+    when 4
+      user.exit_program
+    end
   end
 end
-# welcome
--- bin/run.rb
+++ bin/run.rb
@@ -3,12 +3,18 @@
 require_relative '../config/environment.rb'
 require_relative 'command_line_interface.rb'
 
-puts "HELLO WORLD"
+
 welcome = Welcome.new
 email = welcome.get_email
 user = welcome.find_or_create_user(email)
 
-menu = Menu.new
-choice = menu.choose_what_to_do
+Menu.main_menu(user)
+# review1 = Review.create(user_id: 1,beer_id: 1, rating: 6.0, description: "Awesome")
+# review1 = Review.create(user_id: 1,beer_id: 2, rating: 10.0, description: "Awesome")
+# review1 = Review.create(user_id: 1,beer_id: 2, rating: 10.0, description: "Awesome")
+# review1 = Review.create(user_id: 1,beer_id: 3, rating: 6.0, description: "Awesome")
+# review1 = Review.create(user_id: 1,beer_id: 4, rating: 2.0, description: "Awesome")
+# review1 = Review.create(user_id: 1,beer_id: 2, rating: 10.0, description: "Awesome")
 
-# binding.pry
+
+binding.pry
