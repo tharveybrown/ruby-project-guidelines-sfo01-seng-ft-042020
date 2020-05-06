cc1a1288c20ce2c9dea6a4d13b814190b6c83be3
tharveybrown <tharveybrown@gmail.com>
2020-05-06 14:56:23 -0700

    seed data for users and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,29 @@
-# Beer.destroy_all
 
+# remove data from tables at runtime
 Beer.destroy_all
 User.destroy_all
 FoodPairing.destroy_all
 Review.destroy_all
-# Beer_Food_Pairing.destroy_all
 
 
+
+#Beer Table
 beers = PunkBeer.get_data
-# creates a new instance in Ruby and commits that transaction to the database
-# it is running .new + .save together
+
 beers.each do |beer|
   beer = Beer.new(beer)
   beer.save
 end
 
+# Map food pairing to Beer Id
 food_and_beer_id = FoodPairingApi.get_data.map do |pairing|
   beer_name = pairing[:name]
   beer_id = Beer.find_by(name: beer_name)[:id]
   {beer: beer_id, food_pairings: pairing[:food_pairings]}
-
 end
 
-
-food_and_beer_id.each do |h|
-  
+# FoodPairing table, then insert each food item to corresponding beer
+food_and_beer_id.each do |h|  
   beer = Beer.find_by_id(h[:beer])
   food_list = h[:food_pairings]
   food_list.each do |item|
@@ -32,6 +31,28 @@ food_and_beer_id.each do |h|
     food.save
     beer.food_pairings << food
   end
+end
+
+#User Table
+20.times do 
+  User.create({
+    name: Faker::FunnyName.three_word_name,
+    email: Faker::Internet.email
+  })
+end
+
+
+
+#Review Table.. 
+#use "hipster sentences" from faker to generate descriotions
+
+20.times do 
+  Review.create({user_id: User.all.sample.id, 
+            beer_id: Beer.all.sample.id,
+            rating: rand(0.0..10.0),
+            description: Faker::Hipster.sentences
+            }
+          )
 end
 
 puts "done!"
