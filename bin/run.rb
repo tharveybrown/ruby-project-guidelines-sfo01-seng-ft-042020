6e6d71a39f1aab3714f79a29e2d8fd5019c4cec7
tharveybrown <tharveybrown@gmail.com>
2020-05-06 14:33:34 -0700

    remove get request

--- bin/run.rb
+++ bin/run.rb
@@ -4,8 +4,6 @@ require_relative '../config/environment.rb'
 require_relative 'command_line_interface.rb'
 
 
-PunkBeer.get_data
-
 welcome = Welcome.new
 user = welcome.welcome_user
 Menu.main_menu(user)
