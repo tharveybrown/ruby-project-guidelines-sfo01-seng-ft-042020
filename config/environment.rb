adbc7f101b615a15d7120d77a8b88bd3fadc6e8d
tharveybrown <tharveybrown@gmail.com>
2020-05-06 10:05:37 -0700

    remove logs from run file but keep in console

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,9 @@ ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3', 
   database: 'db/development.db'
 )
-ActiveRecord::Base.logger.level = 1
+# ActiveRecord::Base.logger.level = 1
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger.level = 1 
 
 
 PROMPT = TTY::Prompt.new
